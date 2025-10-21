3187140a3cf10eb2c6b71199d971b8cf9e2dbb53
dangnhien06 <nhiendang2k6@gmail.com>
2025-10-21 08:22:43 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+#Danh sách để lưu các công việc
+Task = []
+Def add_task(task_name):
+    “““Thêm một công việc mới vào danh sách.”””
+    Tasks.append(task_name)
+    Print(f”Đã thêm công việc:’{task name}’’’)
+#--- Điểm bắt đầu của chương tình ---
+If __name__==”__main__”:
+    Print(“chào mừng đến với ứng dụng To-Do List!”)
+    Add_task(“Học bài Git và GitHub”)
+    Add_task(“Làm bài tập thực hành ở nhà”) 
